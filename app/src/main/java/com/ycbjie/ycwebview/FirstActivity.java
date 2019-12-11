beb60d743e6b5c03c11d0673193102d42f2c2af4
Seachal <seachalzhang@gmail.com>
2019-12-11 18:42:13 +0800

    添加了一个注释

--- app/src/main/java/com/ycbjie/ycwebview/FirstActivity.java
+++ app/src/main/java/com/ycbjie/ycwebview/FirstActivity.java
@@ -56,8 +56,8 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
         webView = findViewById(R.id.web_view);
-
-        String url = "https://github.com/yangchong211";
+//        String url = "https://github.com/yangchong211";
+        String url = "https://mobile.yangkeduo.com/duo_coupon_landing.html?goods_id=54692045397&pid=9283978_120621399&customParameters=100123350538&cpsSign=CC_191120_9283978_120621399_c60cc9ffe8bbba684dd0abf1d27d4896&duoduo_type=2";
         webView.loadUrl(url);
     }
 }
